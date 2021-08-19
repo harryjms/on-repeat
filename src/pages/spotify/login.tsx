b922d7fe7ff070af225ca8d45ce947ce02126f24
Harry Jarman <me@harryjms.uk>
2021-08-19 23:45:41 +0100

    Login privacy notice

--- src/pages/spotify/login.tsx
+++ src/pages/spotify/login.tsx
@@ -8,16 +8,35 @@ const SpotifyLogin = ({ loginURL }) => {
   return (
     <main>
       <div className="absolute top-0 right-0 bottom-0 left-0 flex items-center justify-center">
-        <button
-          onClick={() => {
-            window.location = loginURL;
-          }}
-          className="bg-green-400 hover:bg-green-600 text-white p-4 font-bold text-lg transition-colors"
-          style={{ borderRadius: 40 }}
-        >
-          <FontAwesomeIcon icon={faSpotify} className="mr-2" />
-          Login with Spotify
-        </button>
+        <div className="p-4 text-center">
+          <h1>
+            Hello, welcome to <span className="text-brand">On Repeat</span>
+          </h1>
+          <div className="text-sm p-5">
+            <p>
+              This service will display your top listened to tracks from
+              Spotify.
+            </p>
+            <p className="my-2">
+              To ensure your privacy, the authorisation data from Spotify is
+              encrypted and stored as a cookie in your browser.
+            </p>
+            <p className="my-2">
+              Unless you decide to share the list, none of the data generated is
+              stored on our servers.
+            </p>
+          </div>
+          <button
+            onClick={() => {
+              window.location = loginURL;
+            }}
+            className="mt-4 bg-green-400 hover:bg-green-600 text-white p-4 font-bold text-lg transition-colors"
+            style={{ borderRadius: 40 }}
+          >
+            <FontAwesomeIcon icon={faSpotify} className="mr-2" />
+            Login with Spotify
+          </button>
+        </div>
       </div>
     </main>
   );
