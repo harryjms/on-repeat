c5ef6347760e76fb9dc240d7cc1561decb8816aa
Harry Jarman <me@harryjms.uk>
2021-08-20 00:43:23 +0100

    Fix page on modal

--- src/components/ModalWrapper.tsx
+++ src/components/ModalWrapper.tsx
@@ -1,11 +1,17 @@
-import { ReactPortal } from "react";
+import { ReactPortal, useEffect } from "react";
 import ReactDOM from "react-dom";
 
 const ModalWrapper =
   (Component: any) =>
-  ({ onDismiss, ...props }): ReactPortal =>
-    ReactDOM.createPortal(
-      <div className="absolute top-0 right-0 bottom-0 left-0 flex items-center justify-center z-50 bg-black bg-opacity-75">
+  ({ onDismiss, ...props }): ReactPortal => {
+    useEffect(() => {
+      document.getElementsByTagName("body")[0].style.overflowY = "hidden";
+      return () => {
+        document.getElementsByTagName("body")[0].style.overflowY = "";
+      };
+    }, []);
+    return ReactDOM.createPortal(
+      <div className="fixed top-0 right-0 bottom-0 left-0 flex items-center justify-center z-50 bg-black bg-opacity-75">
         <div className="m-4 p-6 dark:bg-gray-700 dark:text-white bg-white text-black rounded-xl text-center">
           <Component {...props} />
           {onDismiss && (
@@ -17,5 +23,6 @@ const ModalWrapper =
       </div>,
       document.getElementById("modal")
     );
+  };
 
 export default ModalWrapper;
