32bc3619c1b9f1f8efc3c974a63a8f0d5e279580
Harry Jarman <me@harryjms.uk>
2021-08-19 23:35:57 +0100

    Title

--- src/pages/spotify/index.tsx
+++ src/pages/spotify/index.tsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import { useRouter } from "next/dist/client/router";
 import React, { useEffect, useState } from "react";
 import Track from "../../components/Track";
+import Head from "next/head";
 
 const SpotifyHomepage = () => {
   const [user, setUser] = useState(null);
@@ -35,46 +36,51 @@ const SpotifyHomepage = () => {
   }, [term]);
 
   return (
-    <main>
-      <div className="p-4">
-        {user && (
-          <div className="text-xl">Hi {user.display_name.split(" ")[0]}.</div>
-        )}
-        <h1>
-          Here are your top tracks on{" "}
-          <span className="text-green-400">Spotify</span>
-        </h1>
-      </div>
-      <div className="sticky top-0 z-10 p-4 dark:bg-black bg-white pt-0">
-        <div className="rounded-lg dark:bg-gray-800 bg-gray-200 p-2">
-          Over the last{" "}
-          <select
-            value={term}
-            onChange={(e) => {
-              setTerm(e.target.value);
-            }}
-            className="bg-transparent"
-          >
-            <option value="short_term">4 weeks</option>
-            <option value="medium_term">6 months</option>
-            <option value="long_term">several years</option>
-          </select>
+    <>
+      <Head>
+        <title>Spotify Top Track - On Repeat</title>
+      </Head>
+      <main>
+        <div className="p-4 pb-0">
+          {user && (
+            <div className="text-xl">Hi {user.display_name.split(" ")[0]}.</div>
+          )}
+          <h1>
+            Here are your top tracks on{" "}
+            <span className="text-green-400">Spotify</span>
+          </h1>
         </div>
-      </div>
-      <div className="p-4 pt-0">
-        {loadingTracks ? (
-          "Loading..."
-        ) : (
-          <ul className="grid grid-cols-1 md:grid-cols-3 gap-4">
-            {tracks.map((track) => (
-              <li key={track.id}>
-                <Track track={track} withPreview />
-              </li>
-            ))}
-          </ul>
-        )}
-      </div>
-    </main>
+        <div className="sticky top-0 z-10 p-4 dark:bg-black bg-white">
+          <div className="rounded-lg dark:bg-gray-800 bg-gray-200 p-2">
+            Over the last{" "}
+            <select
+              value={term}
+              onChange={(e) => {
+                setTerm(e.target.value);
+              }}
+              className="bg-transparent"
+            >
+              <option value="short_term">4 weeks</option>
+              <option value="medium_term">6 months</option>
+              <option value="long_term">several years</option>
+            </select>
+          </div>
+        </div>
+        <div className="p-4 pt-0">
+          {loadingTracks ? (
+            "Loading..."
+          ) : (
+            <ul className="grid grid-cols-1 md:grid-cols-3 gap-4">
+              {tracks.map((track) => (
+                <li key={track.id}>
+                  <Track track={track} withPreview />
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+      </main>
+    </>
   );
 };
 
